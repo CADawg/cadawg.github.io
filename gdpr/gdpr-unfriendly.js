de27abedd9f540c00bc0311de2ae9e957bbeea5a
CA_Dawg <conor.howland@outlook.com>
2018-08-07 11:04:21 +0100

    Create gdpr-unfriendly.js

---
+++ gdpr/gdpr-unfriendly.js
@@ -0,0 +1,11 @@
+gdprUnfriendlyCode = function(allowed = null) {
+            if (allowed!=null) {
+                allowed = getCookie("likescookies");
+            }
+            if (allowed.indexOf("1")) !== -1) {
+                //Social Allowed
+            }
+            if (allowed.indexOf("2")) !== -1) {
+                //Analytics Allowed
+            }
+        }
