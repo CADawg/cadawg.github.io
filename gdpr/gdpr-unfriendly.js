aca360a998af5dcfff7fdc32f21241377792c9fe
CA_Dawg <conor.howland@outlook.com>
2018-08-07 11:08:45 +0100

    Update gdpr-unfriendly.js

--- gdpr/gdpr-unfriendly.js
+++ gdpr/gdpr-unfriendly.js
@@ -2,10 +2,10 @@ gdprUnfriendlyCode = function(allowed = null) {
             if (allowed!=null) {
                 allowed = getCookie("likescookies");
             }
-            if (allowed.indexOf("1")) !== -1) {
+            if (allowed.indexOf("1") !== -1) {
                 //Social Allowed
             }
-            if (allowed.indexOf("2")) !== -1) {
+            if (allowed.indexOf("2") !== -1) {
                 //Analytics Allowed
             }
         }
